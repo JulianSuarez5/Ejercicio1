7a3d8d948108e1b229ef994f6b46bc512e78354c
JulianSuarez5 <Juacosuarez12@gmail.com>
2023-09-04 22:24:23 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class SumaDosNumeros
+{
+    static void Main()
+    {
+        int num1, num2, suma;
+
+        Console.Write("Ingrese el primer número: ");
+        num1 = int.Parse(Console.ReadLine());
+        Console.Write("Ingrese el segundo número: ");
+        num2 = int.Parse(Console.ReadLine());
+
+        suma = num1 + num2;
+
+        Console.WriteLine("La suma es: " + suma);
+        Console.ReadKey();
+    }
+}
